af0984865607d8109fd80a908eff61711a4887db
Alexarj <fcmggo@gmail.com>
2026-01-23 09:53:30 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,10 @@
-<script type="module">
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+  const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
+  const SUPABASE_ANON_KEY = "SUA-ANON-KEY";
 
-export const supabase = createClient(
-  "https://SEU-PROJETO.supabase.co",
-  "SUA-ANON-KEY"
-);
+  const supabase = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
 </script>
