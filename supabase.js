c28a515643fe2b17e4574f76e7cd5d7b4816b836
Alexarj <fcmggo@gmail.com>
2026-01-23 09:33:25 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+<script type="module">
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-// URL e ANON KEY do seu projeto
-const SUPABASE_URL = 'https://ynirlpziolginasusolb.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.YOUR_ANON_KEY_AQUI'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(
+  "https://SEU-PROJETO.supabase.co",
+  "SUA-ANON-KEY"
+);
+</script>
