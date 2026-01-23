ed6b4382a067519997ed0b4cab9b1eea396e99ab
Alexarj <fcmggo@gmail.com>
2026-01-23 16:10:52 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
-  const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-  const SUPABASE_ANON_KEY = "SUA-ANON-KEY";
-
-  const supabase = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
-</script>
+// Atualize SEU-PROJETO e SUA-ANON-KEY
+const supabaseUrl = "https://SEU-PROJETO.supabase.co";
+const supabaseKey = "SUA-ANON-KEY";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
